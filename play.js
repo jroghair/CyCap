bd3c7421bde262204d11bcd328ecae892824e020
Bryan Friestad <bryanf@iastate.edu>
2018-01-28 17:58:10 -0600

    successfully integrated Andrew's stuff into the bigger main_page, etc. Bullets and Player are now subclasses of Entity, which will be the type of all displayed objects. Entities are based off of images instead of basic geometry.

---
+++ play.js
@@ -0,0 +1,161 @@
+//global variables
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
+var keys_down = []; //keys being pressed
+var bullets = []; //all bullets
+var player, bullet_p;
+var player_speed = 3, bullet_speed = 10; //pixels
+var mouseX;
+var mouseY;
+var last_shot_time = 0; //don't change
+var time_between_shots = 100; //milliseconds
+var player_image = "player.png";
+var bullet_image = "enemy.png";
+
+
+//this code executes right when the page is loaded
+setup(); //only call setup once
+
+
+
+//all functions
+function setup() {
+  setInterval(draw, 1000 / 60); //called 60 times a second
+  player = new Player(50, 50, player_image, 0, 0, "recruit", "1");
+  bullet_p = new bullet_population();
+  //setting up two key listeners to improve movement
+  //when a key goes down it is added to a list and when it goes up its taken out
+  document.addEventListener("keydown", function(event) {
+    if (!keys_down.includes(event.keyCode)) {
+      keys_down.push(event.keyCode);
+    }
+  });
+  document.addEventListener("keyup", function(event) {
+    keys_down.splice(keys_down.indexOf(event.keyCode), 1);
+  });
+  //mouse listener for coordinates
+  window.addEventListener('mousemove', getMousePosition, false);
+}
+
+function draw() {
+  context.beginPath(); //so styles dont interfere
+  context.clearRect(0, 0, canvas.width, canvas.height); //clear the canvas
+  player.update();
+  bullet_p.move_bullets();
+  player.draw();
+  bullet_p.draw();
+  context.closePath(); //so styles dont interfere
+}
+
+function getMousePosition(event) {
+  this.rect = canvas.getBoundingClientRect();
+  mouseX = event.clientX - rect.left;
+  mouseY = event.clientY - rect.top;
+}
+
+function toRadians(angle) {
+  return (angle * (Math.PI / 180.0));
+}
+
+function toDegrees(angle) {
+  return (angle * (180.0 / Math.PI));
+}
+
+function Entity(width, height, img, x, y){
+	this.image = new Image();
+	this.image.src = img;
+	this.width = width;
+	this.height = height;
+	//this.speedX = 0; not sure if we need speed right now, so im taking it out
+	//this.speedY = 0; 
+	this.x = x;
+	this.y = y;
+	
+	this.draw = function(){
+		context.drawImage(this.image, this.x, this.y, this.width, this.height);
+	}
+}
+
+function Bullet(width, height, img, x, y) {
+	this.base = Entity;
+	this.base(width, height, img, x, y);
+
+	this.x_diff = mouseX - player.x;
+	this.y_diff = (mouseY - player.y) * -1;
+
+	this.angle = toDegrees(Math.atan(this.y_diff / this.x_diff));
+
+	if (this.x_diff < 0 && this.y_diff > 0) {
+		this.angle += 180;
+	}
+	else if (this.x_diff < 0 && this.y_diff < 0) {
+		this.angle += 180;
+	}
+	else if (this.x_diff > 0 && this.y_diff < 0) {
+		this.angle += 360;
+	}	
+	//console.log(this.angle);
+
+	this.y_ratio = Math.sin(toRadians(this.angle)) * -1;
+	this.x_ratio = Math.cos(toRadians(this.angle));
+	//console.log(this.x_ratio + ',' + this.y_ratio);
+	this.y_ratio += ((Math.random() - 0.5) * 0.05);
+	this.x_ratio += ((Math.random() - 0.5) * 0.05);
+	//console.log(this.x_ratio + ',' + this.y_ratio);
+}
+
+
+function bullet_population() {
+	this.draw = function() {
+		for (var i = 0; i < bullets.length; i++) {
+		    bullets[i].draw();
+		}	
+	}
+
+	this.move_bullets = function() {
+		for (var i = 0; i < bullets.length; i++) {
+			//console.log(bullets[i].x_ratio + ', ' + bullets[i].y_ratio);
+			bullets[i].x += bullet_speed * bullets[i].x_ratio;
+			bullets[i].y += bullet_speed * bullets[i].y_ratio;
+			if (bullets[i].x < 0 || bullets[i].x > canvas.width || bullets[i].y < 0 || bullets[i].y > canvas.height) {
+				bullets.splice(i, 1);
+			}
+		}
+	}
+}
+
+function Player(width, height, img, x, y, role, team) {
+	this.base = Entity;
+	this.base(width, height, img, x, y);
+	this.role = role;
+	this.team = team;
+	//decide health based on role
+	this.has_flag = false;
+	this.mov_speed = player_speed; //this will eventually be dependent on role 
+
+	this.update = function() {
+		if (keys_down.includes(87)) {
+			this.y -= this.mov_speed;
+		}
+		if (keys_down.includes(65)) {
+			this.x -= this.mov_speed;
+		}
+		if (keys_down.includes(68)) {
+			this.x += this.mov_speed;
+		}
+		if (keys_down.includes(83)) {
+			this.y += this.mov_speed;
+		}
+
+		if (keys_down.includes(32)) {
+			if (last_shot_time == 0) {
+				bullets.push(new Bullet(5, 5, bullet_image, this.x, this.y));
+				last_shot_time = Date.now();
+			}
+			else if ((Date.now() - last_shot_time) >= time_between_shots) {
+				bullets.push(new Bullet(5, 5, bullet_image, this.x, this.y));
+				last_shot_time = Date.now();
+			}
+		}
+	}
+}
