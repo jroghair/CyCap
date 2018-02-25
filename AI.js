5e5f19d372ab51a9601b56ba8b6aa058c5424e07
hancocka <hancocka@iastate.edu>
2018-02-25 16:04:59 -0600

    updated demo AI

--- AI.js
+++ AI.js
@@ -1,4 +1,14 @@
+/*
+Creator:
+  Andrew Hancock
+Purpose:
+  This file contains all of the computer player functionality. This is where
+  node generation happens for the map. Also where path generation happens.
+*/
+
+//this is the resolution of the node map. pixels in between nodes.
 var node_pixel_dist = 5;
+
 
 function generateNodes() {
   //go through all coordinates
@@ -20,177 +30,115 @@ function generateNodes() {
     }
     nodes.push(node_col);
   }
-  // console.log('nodal generation time: ' + (Date.now() - time1) + ' ms');
   return 'success';
 }
 
 function aStarPath(moving_point, target_point) {
-	// console.log('starting path...');
   var start_node = nodes[moving_point.x][moving_point.y];
   var goal_node = nodes[target_point.x][target_point.y];
-  // console.log('from: ' + start_node.x + ',' + start_node.y);
-  // console.log('to: ' + goal_node.x + ',' + goal_node.y);
   var open = [];
   open.push(start_node);
   var closed = [];
   var current_node = undefined;
   start_node.g = 0;
   start_node.f = start_node.g + heuristic(start_node, goal_node);
-  //console.log(start_node.f);
   while (open.length != 0) {
-		//start with node with lowest cost function
-    //console.log('open list: ' + open);
     current_node = getLowestF(open);
-    // console.log('current node: ' + current_node.x + ',' + current_node.y);
-    //current_node.print_prev();
     if (current_node === goal_node) {
-      // console.log('path is complete.');
       var path = constructPath(current_node);
       return path;
     }
-		open.splice(getIndex(open, current_node), 1);
-		closed.push(current_node);
-		var neighbors = getNeighbors(current_node);
-		//console.log(neighbors);
-		for(var i = 0;i < neighbors.length;i++){
+    open.splice(getIndex(open, current_node), 1);
+    closed.push(current_node);
+    var neighbors = getNeighbors(current_node);
+    for (var i = 0; i < neighbors.length; i++) {
       var neighbor = neighbors[i];
-			//if the node isnt in visited list
-			if(closed.includes(neighbor) != true){
-				//console.log('doesnt contain');
-				//set cost function for the node
+      if (closed.includes(neighbor) != true) {
 
 
         var temp_gscore = current_node.g + 1;
-        if(open.includes(neighbor)){
-          if(temp_gscore < neighbor.g){
+        if (open.includes(neighbor)) {
+          if (temp_gscore < neighbor.g) {
             neighbor.g = temp_gscore;
           }
-        }else{
+        } else {
           neighbor.g = temp_gscore
           open.push(neighbor);
         }
         neighbor.h = heuristic(neighbor, goal_node);
         neighbor.f = neighbor.g + neighbor.h;
         neighbor.previous = current_node;
-        //console.log('setting: ' + current_node.x + ' as a previous of: ' + neighbor.x);
-
-
-
-        /*
-				neighbor.f = neighbor.g + heuristic(neighbor, goal_node);
-				//if its not in the open list
-				if(node_contains(neighbor, open) != true){
-					open.push(neighbor); //add to open list and go to next iter
-				}else{//if it is in open list get it and set it to openneighbor
-					var openneighbor = getNodeFromList(open, neighbor);
-					if(neighbor.g < openneighbor.g){
-						openneighbor.g = neighbor.g;
-						openneighbor.previous = neighbor.previous;//add to the path
-					}
-				}
-        */
-			}
-		}
+      }
+    }
   }
-
-	// console.log('success');
 }
 
-function getNodeFromList(arr, node){
-	for(var i = 0;i < arr.length;i++){
-		if(arr[i].x == node.x && arr[i].y == node.y){
-			return arr[i];//return node
-		}
-	}
+function getNodeFromList(arr, node) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].x == node.x && arr[i].y == node.y) {
+      return arr[i];
+    }
+  }
 }
 
-function node_contains(arr, node){
-	for(var i = 0;i < arr.length;i++){
-		if(arr[i].x == node.x && arr[i].y == node.y){
-			return true;
-		}
-	}
-	return false;
+function node_contains(arr, node) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].x == node.x && arr[i].y == node.y) {
+      return true;
+    }
+  }
+  return false;
 }
 
-function getNeighbors(node){
-	var nindex = getNearestNode(node);
-	var neighbors = [];
-  if(nodes[nindex.x-1][nindex.y-1].trav == true){
-	   neighbors.push(nodes[nindex.x-1][nindex.y-1]);
+function getNeighbors(node) {
+  var nindex = getNearestNode(node);
+  var neighbors = [];
+  if (nodes[nindex.x - 1][nindex.y - 1].trav == true) {
+    neighbors.push(nodes[nindex.x - 1][nindex.y - 1]);
   }
-  if(nodes[nindex.x-1][nindex.y].trav == true){
-	   neighbors.push(nodes[nindex.x-1][nindex.y]);
+  if (nodes[nindex.x - 1][nindex.y].trav == true) {
+    neighbors.push(nodes[nindex.x - 1][nindex.y]);
   }
-  if(nodes[nindex.x-1][nindex.y+1].trav == true){
-	   neighbors.push(nodes[nindex.x-1][nindex.y+1]);
+  if (nodes[nindex.x - 1][nindex.y + 1].trav == true) {
+    neighbors.push(nodes[nindex.x - 1][nindex.y + 1]);
   }
-  if(nodes[nindex.x][nindex.y+1].trav == true){
-	   neighbors.push(nodes[nindex.x][nindex.y+1]);
+  if (nodes[nindex.x][nindex.y + 1].trav == true) {
+    neighbors.push(nodes[nindex.x][nindex.y + 1]);
   }
-  if(nodes[nindex.x+1][nindex.y+1].trav == true){
-	   neighbors.push(nodes[nindex.x+1][nindex.y+1]);
+  if (nodes[nindex.x + 1][nindex.y + 1].trav == true) {
+    neighbors.push(nodes[nindex.x + 1][nindex.y + 1]);
   }
-  if(nodes[nindex.x+1][nindex.y].trav == true){
-	   neighbors.push(nodes[nindex.x+1][nindex.y]);
+  if (nodes[nindex.x + 1][nindex.y].trav == true) {
+    neighbors.push(nodes[nindex.x + 1][nindex.y]);
   }
-  if(nodes[nindex.x+1][nindex.y-1].trav == true){
-	   neighbors.push(nodes[nindex.x+1][nindex.y-1]);
+  if (nodes[nindex.x + 1][nindex.y - 1].trav == true) {
+    neighbors.push(nodes[nindex.x + 1][nindex.y - 1]);
   }
-  if(nodes[nindex.x][nindex.y-1].trav == true){
-	   neighbors.push(nodes[nindex.x][nindex.y-1]);
+  if (nodes[nindex.x][nindex.y - 1].trav == true) {
+    neighbors.push(nodes[nindex.x][nindex.y - 1]);
   }
-  //set travel costs to nodes
-  for(var i = 0;i < neighbors.length;i++){
-    //neighbors[i].previous = node;
+  for (var i = 0; i < neighbors.length; i++) {
     neighbors[i].g = node.g + 1;
   }
-	return neighbors;
+  return neighbors;
 }
 
-function getIndex(open, node){
-	for(var i = 0;i < open.length;i++){
-		if(open[i].x == node.x && open[i].y == node.y){
-			return i;
-		}
-	}
+function getIndex(open, node) {
+  for (var i = 0; i < open.length; i++) {
+    if (open[i].x == node.x && open[i].y == node.y) {
+      return i;
+    }
+  }
 }
 
 function constructPath(enode) {
-  /*
-  var c_node = snode;
-	console.log('constructing path coordinates...');
-  var path = [];
-  for(;;){
-    path.push(c_node);
-    if(c_node.x == enode.x){
-      if(c_node.y == enode.y){
-        break;
-      }
-    }
-    c_node = c_node.previous;
-    console.log(path);
-    if(path.length > 100){
-      break;
-    }
-  }
-
-  */
   var path = [];
   var temp = enode;
   path.push(temp);
-  // console.log('here1');
-  while(temp.previous){
+  while (temp.previous) {
     path.push(temp.previous);
-    // if(temp.previous == undefined){
-    //   break;
-    // }
-    //console.log('pushed previous');
-    temp = temp.previous;0
-
+    temp = temp.previous;
   }
-  // console.log('here2');
-  // console.log(path);
   return path;
 }
 
@@ -205,14 +153,8 @@ function getLowestF(open) {
 }
 
 function getNearestNode(ent) {
-  // this will round to the nearest 5 since thats where nodes are
   this.x = (Math.ceil((ent.x / node_pixel_dist)) * node_pixel_dist);
-  //console.log('old x: ' + ent.x + ' new x: ' + this.x);
   this.y = (Math.ceil((ent.y / node_pixel_dist)) * node_pixel_dist);
-  //console.log('old y: ' + ent.y + ' new y: ' + this.y);
-
-  //go through all nodes and see if one matches
-  //this could be improved
   var i = 0,
     j = 0;
   while ((nodes[i][j].y != this.y) || (nodes[i][j].x != this.x)) {
@@ -223,7 +165,6 @@ function getNearestNode(ent) {
       j++;
     }
   }
-  //console.log('found node: ' + nodes[i][j].x + ',' + nodes[i][j].y);
   return (new point(i, j));
 }
 
@@ -246,7 +187,7 @@ function node(x, y, trav) {
   this.g = 1;
   this.h;
 
-  this.print_prev = function(){
+  this.print_prev = function() {
     console.log(this.previous);
   }
 }
